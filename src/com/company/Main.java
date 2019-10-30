63df9d62f8b290390cefb0473c1dad6a2832ba20
Samatbek89 <sambrero899@gmail.com>
2019-10-30 20:24:32 +0600

    Home work 2 Samat

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) { // основное задание
+        String name = "Самат";
+        System.out.println(name);
+
+        int myAge = 29;
+        System.out.println(myAge);
+
+        int temperature = 20;
+
+
+        if (myAge == 20-45 && temperature < +35 && temperature > -20);{
+            System.out.println("Можно идти гулять");
+        }
+
+        if (myAge < 20 && temperature > 0 && temperature < 28);{
+            System.out.println("Можно гулять");
+        }
+
+        if (myAge > 45 && temperature > -10 && temperature < 25);{
+            System.out.println("Можно гулять");
+        }
+
+
+
+
+
+
+    }
+}
